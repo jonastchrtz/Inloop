ad65c5b85501d76ddeee361b8a70ed0fa3cad2dc
impact lg <49949786+impact-lg@users.noreply.github.com>
2019-04-28 14:37:06 +0200

    added lesson 5

---
+++ src/lesson5/Book.java
@@ -0,0 +1,54 @@
+package lesson5;
+
+public class Book implements Comparable<Book> {
+
+    private String isbn;
+    private String author = "";
+    private String title = "";
+
+    public Book(String isbn, String author, String title) {
+
+        this.isbn = isbn;
+        this.author = author;
+        this.title = title;
+
+    }
+
+    public Book(String isbn) {
+
+        this.isbn = isbn;
+
+    }
+
+    public String getIsbn() {
+
+        return isbn;
+
+    }
+
+    public String getAuthor() {
+
+        return author;
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public String toString() {
+
+        return title + "; " + author + " (" + isbn + ")";
+
+    }
+
+    public int compareTo(Book b) {
+
+        return getIsbn().compareTo(b.getIsbn());
+
+    }
+
+
+}
