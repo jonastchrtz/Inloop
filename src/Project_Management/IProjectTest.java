16b2cd5573b08d045906b9a6be212fc307281d27
impact lg <49949786+impact-lg@users.noreply.github.com>
2019-07-02 14:23:08 +0200

    added Project Management

---
+++ src/Project_Management/IProjectTest.java
@@ -0,0 +1,12 @@
+package Project_Management;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IProjectTest {
+    @Test
+    public void testInterface() {
+        assertTrue(IProject.class.isInterface());
+    }
+}
